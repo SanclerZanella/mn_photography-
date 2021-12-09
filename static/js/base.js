8c59653aad8b25323f34e76fc199514cb66125ee
Sancler Zanella <sanclerzjj@gmail.com>
2021-12-09 17:44:57 +0000

    Change mobile navbar style

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,17 @@
+// Mobile nav icon
+$('#expand-menu-icon').click(() => {
+    $('.mobile-nav').removeClass('display-mobile-nav');
+    $('.mobile-nav').show(300);
+    $('.mobile-nav').children().show(300);
+});
+
+// Cole mobile nav bar
+$('.close-menu').click(() => {
+    $('.mobile-nav').addClass('display-mobile-nav');
+    $('.mobile-nav').hide(300);
+    $('.mobile-nav').children().hide(300);
+});
+
 // Bootstrap Message (from Boutique Ado)
 $('.toast').toast('show');
 

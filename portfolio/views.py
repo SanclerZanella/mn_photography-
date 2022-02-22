1f392f1e328708831d140b3981e1502f4e30a7d6
Sancler Zanella <sanclerzjj@gmail.com>
2022-02-22 14:50:45 +0000

    Created grid layout to album page

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,6 +8,7 @@ def portfolio(request):
     A view to render the portfolio page
     """
     albums = Album.objects.all()
+    catg = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -15,10 +16,34 @@ def portfolio(request):
             albums = Album.objects.filter(category__name=category_key)
             catg = Category.objects.get(name=category_key)
 
+    form = AlbumForm()
+    if request.method == 'POST':
+
+        album_form = AlbumForm(request.POST, request.FILES)
+        if album_form.is_valid():
+            album_form.save()
+
+            album = Album.objects.get(title=request.POST['title'])
+
+            photos = request.FILES.getlist('photos')
+
+            photo_position = 0
+            for photo in photos:
+
+                photo_position += 1
+
+                AlbumPhoto_line_item = AlbumPhoto(
+                                        album=album,
+                                        photos=photo,
+                                        position=photo_position
+                                    )
+                AlbumPhoto_line_item.save()
+
     template = 'portfolio/collection.html'
     context = {
         'albums': albums,
         'catg': catg,
+        'form': form,
     }
 
     return render(request, template, context)
@@ -28,9 +53,17 @@ def album(request, album_id):
     '''
     '''
     album = Album.objects.get(id=album_id)
+
+    all_photos = list()
+    for ab in AlbumPhoto.objects.filter(album=album):
+        all_photos.append(ab.photos)
+
+    photo_group = [all_photos[i:i + 10] for i in range(0, len(all_photos), 10)]
+
     template = 'portfolio/album.html'
     context = {
         'album': album,
+        'photos_gr': photo_group
     }
 
     return render(request, template, context)

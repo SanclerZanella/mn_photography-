803bfb1ba847eeac0beacb9a3049926406b2ef4d
Sancler Zanela Jorge Junior <sanclerzjj@gmail.com>
2022-05-20 02:12:01 +0100

    Created form to add new album

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -3,12 +3,18 @@ from .models import Album, Category
 
 
 class AlbumForm(forms.ModelForm):
+    '''
+    New album form
+    '''
 
     class Meta:
+        '''
+        Change date format
+        '''
         model = Album
         fields = '__all__'
         widgets = {
-            'date': forms.DateInput()
+            'date': forms.DateInput(attrs={'type': 'date'})
         }
 
     photos = forms.ImageField(label='Photos', required=False, widget=forms.FileInput(attrs={'multiple': True}))
@@ -17,5 +23,27 @@ class AlbumForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
+
+        placeholders = {
+            'category': 'Categoria*',
+            'title': 'Título',
+            'pre_desc': 'Pré descrição',
+            'date': 'Data do evento',
+            'place': 'Lugar do Evento',
+            'cover': 'Foto de capa*',
+            'description': 'Descrição do evento',
+            'photos': 'Fotos*',
+        }
+
+        label_fields = ['category', 'title', 'pre_desc', 'date', 'place', 'cover', 'description', 'photos']
+
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['required'] = 'required'
+
+            if field in label_fields:
+                self.fields[field].label = placeholder
+            else:
+                self.fields[field].label = False
---
+++ portfolio/static/portfolio/js/create_album.js
@@ -0,0 +1,28 @@
+function previewPictures(inputId, previewContainer, imgContainerId, imgElId) {
+
+    const uploadedFiles = $(inputId)[0].files;
+    const numberPhotos = Object.keys(uploadedFiles);
+
+    if(numberPhotos.length == 0) {
+        $(previewContainer).hide(500);
+        $(`#${imgElId}`).remove();
+    } else {
+        $(previewContainer).show(500);
+        $(`#${imgElId}`).remove();
+        $(uploadedFiles).each((key, value) => {
+            let img_url = URL.createObjectURL(value);
+            $(`<div>
+                    <img id=${imgElId} class="previewPicture img-centered" src="${img_url}" alt="">
+                </div>`).appendTo(imgContainerId);
+        });
+    };
+
+};
+
+$('#id_cover').change(() => {
+    previewPictures('#id_cover', '.previewCoverImage', '#newAlbumCoverPrev', 'previewCoverPicture')
+});
+
+$('#id_photos').change(() => {
+    previewPictures('#id_photos', '.previewImages', '#newAlbumPhotosPrev', 'previewPictureAlbum')
+});
